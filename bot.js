40df8cd6d911e23202739881ddd40c7dcdef5449
icebotggMC <41833445+icebotggMC@users.noreply.github.com>
2019-01-04 04:42:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ client.on('message', msg => {
 
 client.on('ready', () => {
     console.log('Bot Is Ready Now !');
-    client.user.setGame(`Epic Clan`,"http://twitch.tv/xkilleryt")
+    client.user.setGame(`By:iixlin`,"http://twitch.tv/xkilleryt")
     });
 
 
